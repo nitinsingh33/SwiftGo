58812c00c9b34bc52a8c2966ce7c32fdae897d52
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-12-06 22:45:05 +0530

    .

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require("express-validator")
+const userController = require('../controllers/user.controller');
 
 
 router.post('/register', [
