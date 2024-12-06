32d96bbbe209ab5fb4ddb31e260d0a68e67a9405
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-12-06 23:51:26 +0530

    user create

--- backend/services/user.services.js
+++ backend/services/user.services.js
@@ -7,5 +7,14 @@ model.exports.createUser = async ({
     if (!firstname || !email || !password) {
         throw new Error('All fields are required');
     }
-    const user = userModel.create
+    const user = userModel.create({
+        fullname: {
+            firstname,
+            lastname
+        },
+        email,
+        password
+    })
+
+    return user;
 }
