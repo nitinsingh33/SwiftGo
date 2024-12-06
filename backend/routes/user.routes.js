06664fcb1061eb6084047d9ed611108aece55e4b
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-12-06 22:43:32 +0530

    add code

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require("express-validator")
 
 
-
+router.post('/register', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname')
+])
 
 modele.exports = router;
